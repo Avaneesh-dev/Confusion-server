853e4b47f23d8b453500032ba8e1d880429c61e8
Avaneesh Srivastava <avaneesh2812@gmail.com>
2022-02-17 11:50:50 +0530

    deploy heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : 'mongodb+srv://Avaneesh2812:srivastava@cluster0.6xrzr.mongodb.net/ConFusion',
     'facebook': {
         clientId: '445633713871108',
         clientSecret: 'd13d2d4be60166d3e7f698b7cf6347d0'
--- routes/favoritesRouter.js
+++ routes/favoritesRouter.js
@@ -9,16 +9,19 @@ const authenticate = require('../authenticate');
 favoritesRouter.use(bodyParser.json());
 favoritesRouter.route('/')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200);})
-    .get(cors.cors, authenticate.verifyUser, (req,res,next)=> {
-        Favorites.find({user: {_id:req.user._id}})
-            .populate('user')
-            .populate('dishes')
-            .then((favorites)=> {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(favorites);
-            }, (err)=> next(err))
-            .catch((err)=> next(err))
+    .get(cors.cors, authenticate.verifyUser, (req,res,next) => {
+        Favorites.findOne({user: req.user._id})
+            .then((favorites) => {
+                if (!favorites) {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    return res.json({"exists": false, "favorites": favorites});
+                }
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    return res.json({"exists": true, "favorites": favorites});
+                }, (err) => next(err))
+            .catch((err) => next(err))
     })
     .post(cors.corsWithOptions, authenticate.verifyUser, (req,res,next)=> {
          Favorites.find({user: {_id:req.user._id}})
